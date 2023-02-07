92c85376f38802d31a0b7c087a3e379d6566ae17
Natasha <koopstranatasha@gmail.com>
2023-02-07 14:59:19 +0100

    Java -1

---
+++ 0-2 Javascript/JavaScript Basics/Nathasha/Variables_&_Datatypes_Opdrachten/main.js
@@ -0,0 +1,77 @@
+//Variables & Datatypes - Opdrachten
+//Opdracht 1.1:
+let letOefeningaangepast = "Let Tekst"
+let LetOefeninga = "Let Tekst twee"
+console.log(letOefeningaangepast)
+let letOefening = "Let Tekst"
+console.log(letOefening)
+LetletOefening = "Let Tekst twee" 
+console.log(LetletOefening)
+const constOefening = "Const Tekst";
+console.log(constOefening)
+constOefeningtwee = "Const Tekst Twee!";
+console.log(constOefeningtwee)
+//Opdracht 1.2:
+let typeQuestion = "Number"
+console.log("This is a " + typeof typeQuestion + ", with the value: "+typeQuestion)
+let typeQuestion1 = "true"
+console.log("This is a " + typeof typeQuestion1 + ", with the value: "+typeQuestion1)
+let typeQuestion2 = "false"
+console.log("This is a " + typeof typeQuestion2 + ", with the value: "+typeQuestion2)
+let typeQuestion3 = ""
+console.log("This is a " + typeof typeQuestion3 + ", with the value: "+typeQuestion3)
+let typeQuestion4 = "22"
+console.log("This is a " + typeof typeQuestion4 + ", with the value: "+typeQuestion4)
+let typeQuestion5 = "22n"
+console.log("This is a " + typeof typeQuestion5 + ", with the value: "+typeQuestion5)
+let typeQuestion6 = "Symbol()"
+console.log("This is a " + typeof typeQuestion6 + ", with the value: "+typeQuestion6)
+let typeQuestion7 = "nul"
+console.log("This is a " + typeof typeQuestion7 + ", with the value: "+typeQuestion7)
+let typeQuestion8 = "new Object();"
+console.log("This is a " + typeof typeQuestion8 + ", with the value: "+typeQuestion8)
+let typeQuestion9 = "{}"
+console.log("This is a " + typeof typeQuestion9 + ", with the value: "+typeQuestion9)
+let typeQuestion10 = "function calculateSomething() {}"
+console.log("This is a " + typeof typeQuestion10 + ", with the value: "+typeQuestion10)
+//Opdracht 1.3:
+let someNumber = 5;
+let anotherNumber = "5";
+console.log(someNumber + anotherNumber)
+console.log(someNumber == anotherNumber)
+console.log(someNumber === anotherNumber)
+console.log(someNumber != anotherNumber)
+console.log(someNumber !== anotherNumber)
+//Opdracht 1.4:
+let increment = 1;
+console.log(++increment)
+console.log(increment++)
+let modulo = 24%10;
+console.log(modulo)
+let dividion = 24/10;
+console.log(dividion)
+let SomeNumber = "Tekst";
+console.log(!SomeNumber)
+let SomeNumber1 = 5;
+console.log(!SomeNumber1)
+let SomeNumber2 = Boolean;
+console.log(!SomeNumber2)
+let SomeNumber3 = false;
+console.log(!SomeNumber3)
+let SomeNumber4 = "false";
+console.log(!SomeNumber4)
+let SomeNumber5 = true;
+console.log(!SomeNumber5)
+let SomeNumber6 = "true";
+console.log(!SomeNumber6)
+let SomeNumber7 = null;
+console.log(!SomeNumber7)
+let SomeNumber8 = undefined;
+console.log(!SomeNumber8)
+
+str = "Hello ";
+str += "World !";
+console.log(str)
+let x = 10;
+let y = 8;
+console.log(x-=y)
