5c65d930f83a341ade6b79056e6dc91d194d75f1
carlyy78@gmail.com <carlyyy78@gmail.com>
2023-02-09 15:59:12 +0100

    Javascript Variables&Datatypes

--- 0-2 Javascript/JavaScript Basics/Carla/Variables_&_Datatypes_Opdrachten/main.js
+++ 0-2 Javascript/JavaScript Basics/Carla/Variables_&_Datatypes_Opdrachten/main.js
@@ -1,6 +1,62 @@
-let tekst = "let oefening2"
+1.1
 
-console.log(tekst)
-console.log(tekst)
-console.log(tekst)
-console.log(tekst)
+const constOefening = "const Tekst"
+console.log(constOefening)
+
+let letOefening = "Let Tekst"
+console.log(letOefening)
+
+varOefening = "var Tekst"
+console.log(varOefening)
+
+varOefening = "var Tekst twee"
+console.log(varOefening)
+
+varOefening = "var Tekst drie"
+console.log(varOefening)
+
+
+1.2
+
+let typeQuestion = "number"
+console.log("this is a " + typeof typeQuestion + ",with a value: " + typeQuestion)
+
+
+1.3
+let someNumber2 = 5;
+let anotherNumber = "5"
+console.log(someNumber2 + anotherNumber)
+console.log(someNumber2 == anotherNumber)
+console.log(someNumber2 != anotherNumber)
+console.log(someNumber2 !== anotherNumber)
+
+
+
+
+1.4
+
+let increment = 1;
+console.log(++increment)
+console.log(increment++)
+
+let modulo = 24 % 10;
+console.log(modulo)
+
+let division = 24 / 10;
+console.log(division)
+
+let someNumber = "tekst";
+console.log( ! someNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
