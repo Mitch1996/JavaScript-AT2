7c78dcdeeabe120f9ff53142280966e7f25d0b57
Malika <malikanaitrrami@gmail.com>
2023-02-14 17:04:45 +0100

    String Methods
    Opdracht 1 t/m 3

---
+++ 0-2 Javascript/JavaScript Basics/Malika/String _Methods_Opdrachten/Opdracht 2.1/main.js
@@ -0,0 +1,36 @@
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens)
+
+// 1.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// let Zin1
+// let Zin2
+// Zin1 = SomeKittens.slice(0,42);
+// console.log (Zin1)
+// Zin2 = SomeKittens.slice(44)
+// console.log (Zin2)
+
+
+// 2.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log(SomeKittens.toUpperCase())
+
+
+// 3.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.indexOf("kittens"))
+// Antwoord is 29.
+
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+//console.log (SomeKittens.lastIndexOf("kittens"))
+// Antwoord is 66.
+
+
+// 4.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.charAt(29))
+// Antwoord is letter k.
+
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.charAt(66))
+// Antwoord is ook letter k.
---
+++ 0-2 Javascript/JavaScript Basics/Malika/String _Methods_Opdrachten/Opdracht 2.2/main.js
@@ -0,0 +1,20 @@
+// 1.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.substring(9,3))
+// Je krijgt poes v. Alles van 3 t/m 9 wordt weergegeven.
+
+// 2.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.slice(9,3))
+// Er wordt niks weergegeven.
+
+// 3.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.substring(-3,-9))
+// Er wordt niks weergegeven. (ook geen error)
+
+// 4.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.slice(-3,-9))
+// Er wordt niks weergegeven. (ook geen error)
+
