155a8c3ae0c3a8ba891fc656d5040bdd0b3444b1
Malika <malikanaitrrami@gmail.com>
2023-02-15 13:56:43 +0100

    Javascript: Conditions
    Opdracht 3

---
+++ 0-2 Javascript/JavaScript Basics/Malika/Conditions_Opdrachten/Opdracht 3.2/main.js
@@ -0,0 +1,101 @@
+// 1.
+
+// let age = 20
+// if (age == 3)
+// {console.log("Leeftijd is 3 jaar.")}
+// else if (age > 4)
+// {console.log("Leeftijd is ouder dan 4 jaar.")}
+// else if (age > 11)
+// {console.log("Leeftijd is ouder dan 11 jaar.")}
+// else if (age < 3)
+// {console.log("Leeftijd is jonger dan 3 jaar")}
+
+// 2.
+// Met de variabele 20 krijg je de uitkomst "Leeftijd is ouder dan 4 jaar" en niet "Leeftijd is ouder dan 11 jaar".
+
+
+// 3.
+
+// let maand = 0
+// if (maand == 1)
+// {console.log("Januari")}
+// else if (maand == 2)
+// {console.log("Februari")}
+// else if (maand == 3)
+// {console.log("Maart")}
+// else if (maand == 4)
+// {console.log("April")}
+// else if (maand == 5)
+// {console.log("Mei")}
+// else if (maand == 6)
+// {console.log("Juni")}
+// else if (maand == 7)
+// {console.log("Juli")}
+// else if (maand == 8)
+// {console.log("Augustus")}
+// else if (maand == 9)
+// {console.log("September")}
+// else if (maand == 10)
+// {console.log("Oktober")}
+// else if (maand == 11)
+// {console.log("November")}
+// else if (maand == 12)
+// {console.log("December")}
+// else if (maand == 0 || maand >12)
+// {console.log("Geen geldige maand!")}
+
+
+// 4.
+
+// let maand = 8
+// switch (maand) 
+// {
+//     case 1: maand = "Januari"
+//     break;
+//     case 2: maand = "Februari"
+//     break;
+//     case 3: maand = "Maart" 
+//     break;
+//     case 4: maand = "April" 
+//     break;
+//     case 5: maand = "Mei" 
+//     break;
+//     case 6: maand = "Juni" 
+//     break;
+//     case 7: maand = "Juli" 
+//     break;
+//     case 8: maand = "Augustus" 
+//     break;
+//     case 9: maand = "September" 
+//     break;
+//     case 10: maand = "Oktober" 
+//     break;
+//     case 11: maand = "November" 
+//     break;
+//     case 12: maand = "December" 
+//     break;
+//     default: maand = "Geen geldige maand!"
+// }
+// console.log(maand)
+
+
+// 5.
+
+// let x = 3;
+// let results;
+// if (x<=4 && x>=0) {
+//     results = 2;
+// } else {
+//     results = 5;
+// }
+// console.log(results)
+
+let x = 3
+console.log((x<=4 && x>=0) ? 2 : 5)
+
+
+
+
+
+
+
