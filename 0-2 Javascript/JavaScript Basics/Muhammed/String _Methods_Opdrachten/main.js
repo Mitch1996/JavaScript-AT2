5eb152daba01ca0580243e9983e607aa9b3f6adc
Muhammed <bektasma@gmail.com>
2023-02-16 13:26:10 +0100

    String methods opdracht

---
+++ 0-2 Javascript/JavaScript Basics/Muhammed/String _Methods_Opdrachten/main.js
@@ -0,0 +1,59 @@
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens)
+
+// 1.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// let Zin1
+// let Zin2
+// Zin1 = SomeKittens.slice(0,42);
+// console.log (Zin1)
+// Zin2 = SomeKittens.slice(44)
+// console.log (Zin2)
+
+
+// 2.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log(SomeKittens.toUpperCase())
+
+
+// 3.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.indexOf("kittens"))
+// Antwoord is 29.
+
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+//console.log (SomeKittens.lastIndexOf("kittens"))
+// Antwoord is 66.
+
+
+// 4.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.charAt(29))
+// Antwoord is letter k.
+
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.charAt(66))
+// Antwoord is ook letter k.
+
+// 1.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.substring(9,3))
+// Je krijgt poes v. Alles van 3 t/m 9 wordt weergegeven.
+
+// 2.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.slice(9,3))
+// Er wordt niks weergegeven.
+
+// 3.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.substring(-3,-9))
+// Er wordt niks weergegeven. (ook geen error)
+
+// 4.
+// let SomeKittens = "De poes van de buurman heeft kittens gehad! Hij vraagt of wij nog kittens willen.";
+// console.log (SomeKittens.slice(-3,-9))
+// Er wordt niks weergegeven. (ook geen error)
+
+let someKittens = "Die nieuwe kittens zijn zo schattig!";
+console.log (someKittens.split(" "))
