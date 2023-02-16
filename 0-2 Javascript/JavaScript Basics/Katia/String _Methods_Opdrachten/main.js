b9c0d0c2f04e092bfb5118038c70f72853305a6d
Katia <rootzkaat@gmail.com>
2023-02-16 13:21:45 +0100

    test push

---
+++ 0-2 Javascript/JavaScript Basics/Katia/String _Methods_Opdrachten/main.js
@@ -0,0 +1,6 @@
+// Opdracht 2.1
+let someKittens= "De poes van de buurman heeft kittens gehad!"
+console.log(someKittens)
+
+let someKittens= "Hij vraagt of wij nog kittens willen"
+console.log(someKittens)
