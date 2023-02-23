210bfcf85f27e9038535d79a9362e3ebfd8a57b1
Efisio Cardia <ecardia@ziggo.nl>
2023-02-23 13:33:33 +0100

    Functions

---
+++ 0-2 Javascript/JavaScript Basics/Efisio/Functions_Opdrachten/Opdracht4_2/vraag4.js
@@ -0,0 +1,6 @@
+const nodeList = document.querySelectorAll(".example");
+for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].style.backgroundColor = "red";
+        nodeList[i].style.color = "white";
+        nodeList[i].style.width = "500px";
+}
