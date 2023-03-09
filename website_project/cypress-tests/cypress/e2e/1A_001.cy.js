50893cd2f6c981c71b5fbc7ad2c454a6026b0c2a
Marten <martenbutter@hotmail.com>
2023-03-09 14:50:48 +0100

    Test scripts updated

--- website_project/cypress-tests/cypress/e2e/1A_001.cy.js
+++ website_project/cypress-tests/cypress/e2e/1A_001.cy.js
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Productoverzicht test", () => {
+  it("Navigeer naar productoverzicht en controleer alle producten", () => {
+    cy.visit("https://techgrounds-at2.myshopify.com/collections/all");
+
+    // Wacht tot de pagina is geladen
+    cy.wait(2000);
+    const categoryArray = ["Bags", "Shoes", "Products"]
+
+    cy.get('.list-menu').contains("Producten").click({force: true});
+
+    // categoryArray.forEach((item)=>{
+    //   cy.find(item).contains()
+
+    //   cy.get('.nav').contains('About') 
+    // }
+    // )
+    // Controleer of het element .product-grid bestaat op de pagina
+  //   if (cy.get(".list-menu__item").should("exist")) {
+  //     cy.get(".list-menu__item").each(($item) => {
+  //       console.log($item) // .nav(footer)
+  //     });  
+  // }
+}) // ctrl + /
+});
+
+
+/*
+ 
+*/
--- website_project/cypress-tests/cypress/e2e/1A_003.cy.js
+++ website_project/cypress-tests/cypress/e2e/1A_003.cy.js
@@ -1,5 +1,47 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Test Case: Verify Sort and Filter options on Product page", () => {
+  it("Navigeer naar https://techgrounds-at2.myshopify.com/", () => {
+    cy.visit("https://techgrounds-at2.myshopify.com/");
+  });
+
+  it("Navigeer naar een productoverzicht pagina", () => {
+    cy.visit("https://techgrounds-at2.myshopify.com/");
+    cy.wait(1000);
+    cy.contains("All Products").click({ force: true });
+    cy.url().should("include", "/collections");
+  });
+
+  it("Vergelijk sorteer opties met testdata", () => {
+    cy.visit("https://techgrounds-at2.myshopify.com/collections/all");
+    const sortOptions = [
+      "Featured",
+      "Best selling",
+      "Alphabetically, A-Z",
+      "Alphabetically, Z-A",
+      "Price, low to high",
+      "Price, high to low",
+      "Date, old to new",
+      "Date, new to old",
+    ];
+    console.log(sortOptions);
+    if (sortOptions) {
+      cy.get('select[name="sort_by"]').eq(0).select("best-selling");
+      cy.reload();
+      cy.wait(2000);
+      cy.get('select[name="sort_by"] > option').each(($option, index) => {
+        if (index < sortOptions.length) {
+          expect($option.text().trim()).to.equal(sortOptions[index]);
+        }
+      });
+    } else {
+      cy.log("sortOptions is undefined or not initialized correctly.");
+    }
+  });
+  it("Vergelijk filteropties met testdata.",()=>{
+    cy.visit("https://techgrounds-at2.myshopify.com/collections/all");
+    cy.get("span").contains("Availability").click()
+    const element = cy.get("li[class*='facets__item']")
+    console.log("Gevonden elementen zijn: " + element)
+
+  }
+  );
+});
