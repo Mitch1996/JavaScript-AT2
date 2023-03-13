e63bfd5a6d7c08b7cceaeb64bc05274c38b7b142
Ropvd <ropvdw@gmail.com>
2023-03-13 11:31:41 +0100

    2A 001 aangepast

--- website_project/cypress-tests/cypress/e2e/2A_001.cy.js
+++ website_project/cypress-tests/cypress/e2e/2A_001.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 describe('Testing the Product Details Page', () => {
-    beforeEach(() => {
-           cy.visit('https://techgrounds-at2.myshopify.com')
-    })
-    it('displays producten in menu', () => {
-            cy.get('.header__inline-menu').should('exist')}
-)}
-)
+      
+ 
+   it("Navigeer naar een productoverzicht pagina", () => {
+       cy.visit("https://techgrounds-at2.myshopify.com/");
+       cy.wait(1000);
+       cy.contains("All Products").click({ force: true });
+       cy.url().should("include", "/collections");
+     });
 
+})
