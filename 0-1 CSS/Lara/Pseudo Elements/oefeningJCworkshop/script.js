b3d8e6fbaafa1ddada5f5a9cbd760cf600e741b0
Laratests <lara.naomi@ziggo.nl>
2023-03-13 09:27:30 +0100

    Auto stash before merge of "main" and "origin/main"
    OefeningJS

---
+++ 0-1 CSS/Lara/Pseudo Elements/oefeningJCworkshop/script.js
@@ -0,0 +1,9 @@
+console.log("Hello Techgrounds");
+let naam="Lara";
+console.log(naam);
+let sum = 3 + 4;
+console.log(sum);
+let. sumWithStrings = "3" + "4";
+let modulus = 12% 9;
+console.log(modulus);
+console.log(typeof modulus)
