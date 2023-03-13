dc637fc1787445d3e1fb893aeb79b059a7e1e3f8
Marten <martenbutter@hotmail.com>
2023-03-13 10:12:43 +0100

    1A_002

--- website_project/cypress-tests/cypress/e2e/1A_002.cy.js
+++ website_project/cypress-tests/cypress/e2e/1A_002.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Testen of alle productcategorieen pagina zijn', () => {
+  it('Navigeer naar https://techgrounds-at2.myshopify.com/	', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('Vergelijk de menu waarden met de lijst gegevens van categorieen', () => {
     cy.visit('https://example.cypress.io')
   })
 })
